a833eb366e928969af3b34d63d8c0159a4158c0e
Kayode <dadavictory2000@gmail.com>
2025-02-13 21:29:11 +0100

    Solved error from my context api due to improper handling of the local storage

--- src/components/context/index.jsx
+++ src/components/context/index.jsx
@@ -1,14 +1,32 @@
-'use client'
+"use client";
 
 import { createContext, useContext, useEffect, useState } from "react";
 
 const AppContext = createContext();
 
 export function AppWrapper({ children }) {
-  const [currentStep, setCurrentStep] = useState(JSON.parse(localStorage.getItem('currentStep') || 1));
+  const [currentStep, setCurrentStep] = useState(() => {
+    if (typeof window !== "undefined") {
+      return JSON.parse(localStorage.getItem("currentStep")) || 1;
+    }
+    return 1;
+  });
 
-  const [ticketInfo, setTicketInfo] = useState(
-    JSON.parse(localStorage.getItem("ticketInfo")) || {
+  const [ticketInfo, setTicketInfo] = useState(() => {
+    if (typeof window !== "undefined") {
+      return (
+        JSON.parse(localStorage.getItem("ticketInfo")) || {
+          attendeeDetials: {
+            profileImgUrl: "",
+            name: "",
+            email: "",
+            specialRequest: "",
+          },
+          ticketType: "regular access",
+        }
+      );
+    }
+    return {
       attendeeDetials: {
         profileImgUrl: "",
         name: "",
@@ -16,43 +34,51 @@ export function AppWrapper({ children }) {
         specialRequest: "",
       },
       ticketType: "regular access",
-    }
-  );
+    };
+  });
 
   const [ticketTypes, setTicketTypes] = useState([
     {
-      price: 'free',
-      name: 'regular access',
-      totalAvailable: 50
+      price: "free",
+      name: "regular access",
+      totalAvailable: 50,
     },
     {
-      price: '$150',
-      name: 'vip access',
-      totalAvailable: 50
+      price: "$150",
+      name: "vip access",
+      totalAvailable: 50,
     },
     {
-      price: '$250',
-      name: 'vvip access',
-      totalAvailable: 50
+      price: "$250",
+      name: "vvip access",
+      totalAvailable: 50,
     },
-  ])
+  ]);
 
   useEffect(() => {
-    localStorage.setItem('currentStep', JSON.stringify(currentStep))
-  }, [currentStep])
+    localStorage.setItem("currentStep", JSON.stringify(currentStep));
+  }, [currentStep]);
 
   useEffect(() => {
-    localStorage.setItem('ticketInfo', JSON.stringify(ticketInfo))
-  }, [ticketInfo])
-
+    localStorage.setItem("ticketInfo", JSON.stringify(ticketInfo));
+  }, [ticketInfo]);
 
   return (
-    <AppContext.Provider value={{currentStep, setCurrentStep, ticketInfo, setTicketInfo, ticketTypes, setTicketTypes}}>
+    <AppContext.Provider
+      value={{
+        currentStep,
+        setCurrentStep,
+        ticketInfo,
+        setTicketInfo,
+        ticketTypes,
+        setTicketTypes,
+      }}
+    >
       {children}
     </AppContext.Provider>
-  )
+  );
 }
 
 export function useAppContext() {
-  return useContext(AppContext)
+  return useContext(AppContext);
 }
